db3cbf3b64663d6cc72f297b887a7ce9843fc96c
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2024-09-30 10:22:21 +0500

    Lab 4

---
+++ CS-471/Lab 5/Employee.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<stdio.h>
+
+class Employee
+{
+private:
+    int id;
+    string name;
+    string address;
+public:
+    Employee(/* args */);
+    ~Employee();
+};
